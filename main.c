4458c60248ed5e268283aa3c537d64ad57ba79d7
adept3101 <82520598+adept3101@users.noreply.github.com>
2025-04-15 20:48:56 +0300

    second gen

--- main.c
+++ main.c
@@ -11,8 +11,21 @@ void num_gen(int size){
         pass[i] = rand() % 10;
         printf("%d", pass[i]);
     }
-    int password = *pass;
+    //int password = *pass;
     free(pass);
+    printf("\n");
+
+    return;
+}
+
+void small_symbol(int size){
+    srand(time(NULL));
+    unsigned char *pass_c = malloc(size * sizeof(unsigned char));
+    for(int i = 0; i < size; i++){
+        pass_c[i] = 97 + rand() % (122 - 97 + 1);
+        printf("%c", pass_c[i]);
+    }
+    free(pass_c);
     printf("\n");
 
     return;
@@ -26,5 +39,7 @@ int main(){
     printf("n=%d\n", n);
     num_gen(n);
     printf("\n");
+
+    small_symbol(n);
     return 0;
 }
